8267a0388dad0fa30ad2a92cdc39b4065d426670
ninggaocong <ninggaocong@wondersgroup.com>
2020-03-11 17:26:53 +0800

    提取完成

--- template/spring-boot-fast-demo/src/main/java/com/ninggc/template/springbootfastdemo/project/web/controller/TestController.java
+++ template/spring-boot-fast-demo/src/main/java/com/ninggc/template/springbootfastdemo/project/web/controller/TestController.java
@@ -12,6 +12,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * @author ninggc
+ */
 @RestController
 @RequestMapping("/test")
 @Api(tags = "测试用controller")
